9e08c32edccd6efcaa1b113ed42b7f68aef3711e
Darius <darius.vincent5@gmail.com>
2023-11-16 15:50:15 +0800

    Everything is working for Home Page

--- app/src/main/java/com/mobdeve/s17/catchow/MainActivity.java
+++ app/src/main/java/com/mobdeve/s17/catchow/MainActivity.java
@@ -7,14 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SearchView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -28,10 +31,12 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     ArrayList<Restaurant> restaurantList = new ArrayList<>();
     FirebaseFirestore db;
+    ImageButton profile_btn;
     SearchView res_sv;
     Button res_btn;
     Button coffee_btn;
     Button des_btn;
+    BottomNavigationView navbar;
 
     // Restaurant Recycler View Variables
     RecyclerView restaurant_rv;
@@ -44,13 +49,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Declarations
+        profile_btn = findViewById(R.id.top_profile_btn);
         restaurant_rv = findViewById(R.id.restaurant_rv);
         res_sv = findViewById(R.id.restaurant_search);
         res_btn = findViewById(R.id.res_btn);
         coffee_btn = findViewById(R.id.coffee_btn);
         des_btn = findViewById(R.id.dessert_btn);
+        navbar = findViewById(R.id.navbar);
+
+        //Setup Bottom Navigation View
+        setupBottomNavigationView();
 
         // Setup Search View
+        setupSearchView();
+
+        // Setup Firestore Database
+        db = FirebaseFirestore.getInstance();
+
+        // Setup Restaurant Recycler View
+        setupRestaurantRecyclerView();
+    }
+
+    private void setupBottomNavigationView() {
+        navbar.setSelectedItemId(R.id.menu_home);
+        navbar.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menu_home) {
+                return true;
+            }
+            else if (id == R.id.menu_cart) {
+                startActivity(new Intent(getApplicationContext(),CartActivity.class));
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+                return true;
+            }
+            else if (id == R.id.menu_address) {
+                startActivity(new Intent(getApplicationContext(), AddressActivity.class));
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+                return true;
+            }
+            else if (id == R.id.menu_profile) {
+                startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void setupSearchView() {
         res_sv.clearFocus();
         res_sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -64,12 +113,52 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
 
-        // Setup Firestore Database
-        db = FirebaseFirestore.getInstance();
+    private void setupRestaurantRecyclerView() {
+        db.collection("restaurants")
+                .orderBy("name", Query.Direction.ASCENDING)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                Log.d(TAG, "Success: Get Restaurants Request Successful!");
+                List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
+                for (DocumentSnapshot snapshot: snapshotList) {
+                    Log.d(TAG, "Restaurant: " + snapshot.getData());
+                    Restaurant res = new Restaurant(
+                            snapshot.getString("imageurl"),
+                            snapshot.getString("name"),
+                            snapshot.getString("type"),
+                            snapshot.getString("level"),
+                            snapshot.getDouble("minimum"),
+                            snapshot.getDouble("fee"),
+                            snapshot.getString("duration"),
+                            snapshot.getString("distance"),
+                            new ArrayList<>(), // Initialize empty menu
+                            new ArrayList<>()  // Initialize empty ratings
+                    );
+                    restaurantList.add(res);
+                }
 
-        // Setup Restaurant Recycler View
-        setupRestaurantRecyclerView();
+                // Initialize Recycler View
+                adapter = new Restaurant_RVAdapter(MainActivity.this, restaurantList);
+                restaurant_rv.setAdapter(adapter);
+                horizontalLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+                restaurant_rv.setLayoutManager(horizontalLayoutManager);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "Failure: ", e);
+            }
+        });
+    }
+
+    public void profile(View v) {
+        startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+        overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+        finish();
     }
 
     private void search(String newText) {
@@ -114,45 +203,5 @@ public class MainActivity extends AppCompatActivity {
             }
             adapter.setFilteredList(filteredList);
         }
-    }
-
-    private void setupRestaurantRecyclerView() {
-        db.collection("restaurants")
-                .orderBy("name", Query.Direction.ASCENDING)
-                .get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                Log.d(TAG, "Success: Get Restaurants Request Successful!");
-                List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
-                for (DocumentSnapshot snapshot: snapshotList) {
-                    Log.d(TAG, "Restaurant: " + snapshot.getData());
-                    Restaurant res = new Restaurant(
-                            snapshot.getString("imageurl"),
-                            snapshot.getString("name"),
-                            snapshot.getString("type"),
-                            snapshot.getString("level"),
-                            snapshot.getDouble("minimum"),
-                            snapshot.getDouble("fee"),
-                            snapshot.getString("duration"),
-                            snapshot.getString("distance"),
-                            new ArrayList<>(), // Initialize empty menu
-                            new ArrayList<>()  // Initialize empty ratings
-                    );
-                    restaurantList.add(res);
-                }
-
-                // Initialize Recycler View
-                adapter = new Restaurant_RVAdapter(MainActivity.this, restaurantList);
-                restaurant_rv.setAdapter(adapter);
-                horizontalLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
-                restaurant_rv.setLayoutManager(horizontalLayoutManager);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.e(TAG, "Failure: ", e);
-            }
-        });
     }
 }
