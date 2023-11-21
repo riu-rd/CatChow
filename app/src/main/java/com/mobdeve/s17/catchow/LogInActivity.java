5c13191884538c2fa56e1b8b7d4700aea0f15ce0
monsaur <aurea_sanjuan@dlsu.edu.ph>
2023-11-21 21:34:37 +0800

    Added Login page

---
+++ app/src/main/java/com/mobdeve/s17/catchow/LogInActivity.java
@@ -0,0 +1,33 @@
+package com.mobdeve.s17.catchow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        TextView textView = findViewById(R.id.name_login);
+        String text = "CatChow";
+        Spannable spannable = new SpannableString(text);
+
+        ForegroundColorSpan catColorSpan = new ForegroundColorSpan(Color.parseColor("#FFE4B5"));
+        spannable.setSpan(catColorSpan, 0, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        ForegroundColorSpan chowColorSpan = new ForegroundColorSpan(Color.parseColor("#8B4513"));
+        spannable.setSpan(chowColorSpan, 3, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(spannable);
+
+    }
+}
