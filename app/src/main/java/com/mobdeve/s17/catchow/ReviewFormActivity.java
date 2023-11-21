2ffdcd08d585a526705c1f06ffa532436514d845
monsaur <aurea_sanjuan@dlsu.edu.ph>
2023-11-21 22:48:02 +0800

    Added Review Form file

---
+++ app/src/main/java/com/mobdeve/s17/catchow/ReviewFormActivity.java
@@ -0,0 +1,53 @@
+package com.mobdeve.s17.catchow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ReviewFormActivity extends AppCompatActivity {
+
+    BottomNavigationView navbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_form);
+
+        navbar = findViewById(R.id.navbar);
+
+        //Setup Bottom Navigation View
+        setupBottomNavigationView();
+    }
+
+    private void setupBottomNavigationView() {
+        navbar.setSelectedItemId(R.id.menu_profile);
+        navbar.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menu_home) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+                return true;
+            }
+            else if (id == R.id.menu_cart) {
+                startActivity(new Intent(getApplicationContext(),CartActivity.class));
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+                return true;
+            }
+            else if (id == R.id.menu_address) {
+                startActivity(new Intent(getApplicationContext(), AddressActivity.class));
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+                return true;
+            }
+            else if (id == R.id.menu_profile) {
+                return true;
+            }
+            return false;
+        });
+    }
+}
