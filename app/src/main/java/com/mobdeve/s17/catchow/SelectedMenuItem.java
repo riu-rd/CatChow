028a73e15aa696f5430ab34aec3b3753e21d80b3
Erica <90797144+ericakaye@users.noreply.github.com>
2023-11-26 16:56:27 +0800

    Food info and transaction page

---
+++ app/src/main/java/com/mobdeve/s17/catchow/SelectedMenuItem.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s17.catchow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectedMenuItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_menu_item);
+    }
+}
